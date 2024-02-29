dcbe0d584ac45271d2be16dfbdde4b8a88ec3603
Ittalian <bothmather@au.com>
2024-03-01 02:50:56 +0900

    仮想環境構築

---
+++ venv_spo_auto/Scripts/rst2xml.py
@@ -0,0 +1,23 @@
+#!C:\Users\bothm\dev_Python\kivy_project\Spotify_Automation\venv_spo_auto\Scripts\python.exe
+
+# $Id: rst2xml.py 9115 2022-07-28 17:06:24Z milde $
+# Author: David Goodger <goodger@python.org>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing Docutils XML.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except Exception:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+
+
+description = ('Generates Docutils-native XML from standalone '
+               'reStructuredText sources.  ' + default_description)
+
+publish_cmdline(writer_name='xml', description=description)
