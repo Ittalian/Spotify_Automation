68a88f712722a2b9db55b42058f058318bbdc814
Ittalian <bothmather@au.com>
2024-03-01 17:32:56 +0900

    complete click event

--- main.py
+++ main.py
@@ -2,19 +2,23 @@
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-
-from kivy.properties import StringProperty 
+from kivy.properties import StringProperty, BooleanProperty
 
 class TextWidget(Widget):
     text = StringProperty()    # プロパティの追加
+    is_starting = BooleanProperty(False)
 
     def __init__(self, **kwargs):
         super(TextWidget, self).__init__(**kwargs)
         self.text = ''
 
-    def buttonClicked(self):        # ボタンをクリック時
-        pass
-
+    def check_app_state(self):      # ボタンをクリック時
+        # is_starting = False
+        self.is_starting = not self.is_starting
+        if self.is_starting:
+            self.text = "Starting App"
+        else:
+            self.text = "Stopping App"
 
 class spotify_automationApp(App):
     def __init__(self, **kwargs):
